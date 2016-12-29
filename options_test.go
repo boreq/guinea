ff628b2f2694909eb0c7d32b0c6cc4e107a55d47
boreq <boreq@0x46.net>
2016-12-29 22:05:46 +0100

    Added tests

---
+++ options_test.go
@@ -0,0 +1,33 @@
+package guinea
+
+import (
+	"testing"
+)
+
+func TestOptionShortName(t *testing.T) {
+	opt := Option{Name: "o"}
+	if opt.String() != "-o" {
+		t.Fatal("Invalid:", opt.String())
+	}
+}
+
+func TestOptionLongName(t *testing.T) {
+	opt := Option{Name: "option"}
+	if opt.String() != "--option" {
+		t.Fatal("Invalid:", opt.String())
+	}
+}
+
+func TestArgumentSingularName(t *testing.T) {
+	arg := Argument{Name: "argument", Multiple: false}
+	if arg.String() != "<argument>" {
+		t.Fatal("Invalid:", arg.String())
+	}
+}
+
+func TestArgumentMultipleName(t *testing.T) {
+	arg := Argument{Name: "argument", Multiple: true}
+	if arg.String() != "<argument>..." {
+		t.Fatal("Invalid:", arg.String())
+	}
+}
